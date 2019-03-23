0ae92cb819951b97c8563c606945cd5cc773e7ba
Naveen Thota <naveen.thota@motocho.io>
2019-03-24 00:44:06 +0530

    removed php7.2-mcrypt as it was not found during installation

--- php/recipes/upgrade_to_72.rb
+++ php/recipes/upgrade_to_72.rb
@@ -14,7 +14,7 @@ when "debian", "ubuntu"
 
   execute "apt_install" do
     user "root"
-    command "apt-get -y --force-yes install php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-mcrypt php7.2-mysql php7.2-xml php7.2-curl php7.2-json php7.2-opcache php7.2-zip php7.2-fpm"
+    command "apt-get -y --force-yes install php7.2 php7.2-cli php7.2-common php7.2-mbstring  php7.2-mysql php7.2-xml php7.2-curl php7.2-json php7.2-opcache php7.2-zip php7.2-fpm"
     notifies :run, 'execute[disable_php5_apache]', :delayed
   end
 
