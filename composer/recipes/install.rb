6178636e4e24a3cbada2509da6b2975453c1ba4e
Naveen Thota <naveen.thota@motocho.io>
2019-03-24 19:40:50 +0530

    commented out running composed as root user

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |application, deploy|
   script "install_composer" do
     interpreter "bash"
-    user "root"
+    #user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
